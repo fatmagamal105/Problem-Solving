a3aa880a49db8dc8b88a02103943bea5138ed197
fatmagamal105 <66336409+fatmagamal105@users.noreply.github.com>
2021-04-29 15:58:24 +0200

    Interface EX

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace OOP06N2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Widget w = new Widget();
+            w.First();
+
+
+            ((IWidget)w).Second();
+        }
+    }
+}
---
+++ Widget.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP06
+{
+    class Widget : IWidget
+    {
+        public virtual void First()
+        {
+            Console.WriteLine("inside versiual method");
+        }
+
+        void IWidget.Second()
+        {
+            Console.WriteLine("Inside explicity Method");
+        }
+    }
+}
